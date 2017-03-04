bafa0e3ce573605e6274b66b5768f5e4f091bab0
Nikhil KM <nikhilkm.2296@gmail.com>
2017-03-04 22:50:37 +0530

    Adding add-place page

--- src/components/add-place/add-place.component.ts
+++ src/components/add-place/add-place.component.ts
@@ -2,22 +2,76 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { Slides } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../../components/home/home.component';
+import { PlaceService } from '../../services/places/places.service';
+import {
+  Geolocation,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapsLatLng,
+  CameraPosition,
+  GoogleMapsMarkerOptions,
+  GoogleMapsMarker
+ } from 'ionic-native';
 
 @Component({
   templateUrl: '../../pages/add-place/add-place.html'
 })
 export class AddPlacePage implements OnInit {
 
-   @ViewChild(Slides) slides: Slides;
+  @ViewChild( Slides ) slides: Slides;
+  private _latitude: number;
+  private _longitude: number;
 
-  constructor( private _navController: NavController ) {
+  constructor( private _navController: NavController,
+    private _placeService: PlaceService
+  ) {
   }
 
   ngOnInit() {
     this.slides.lockSwipes( true );
+    let env = this;
+    Geolocation.getCurrentPosition()
+    .then( resp => {
+      env._latitude  = resp.coords.latitude;
+      env._longitude = resp.coords.longitude;
+      env.prepareMap();
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
-  goToNextSlide(): void {
+  prepareMap(): void {
+     let element: HTMLElement = document.getElementById('map');
+     let map = new GoogleMap( element );
+     map.one( GoogleMapsEvent.MAP_READY )
+     .then( () => console.log( 'Map is ready!' ) );
+     let latLng: GoogleMapsLatLng = new GoogleMapsLatLng(
+       this._latitude,
+       this._longitude
+     );
+     let markerOptions: GoogleMapsMarkerOptions = {
+       position: latLng,
+       title: 'Ionic'
+     };
+     map.addMarker( markerOptions )
+    .then( ( marker: GoogleMapsMarker ) => {
+       marker.showInfoWindow();
+     });
+   }
+
+  goToAskName(): void {
+    this.slides.lockSwipes( false );
+    this.slides.slideNext();
+    this.slides.lockSwipes( true );
+  }
+
+  detectLocation(): void {
+    this.slides.lockSwipes( false );
+    this.slides.slideNext();
+    this.slides.lockSwipes( true );
+  }
+
+  goToThankYou(): void {
     this.slides.lockSwipes( false );
     this.slides.slideNext();
     this.slides.lockSwipes( true );
